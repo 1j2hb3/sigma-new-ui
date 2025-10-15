a5303f531d1cf2f9434425b6f58c3fe7bca5a963
1j2hb3 <150550189+1j2hb3@users.noreply.github.com>
2025-10-15 06:02:57 -0400

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -2782,10 +2782,11 @@ function Library.Window(self, Options)
 		slidername.Name = "Slidername"
 		slidername.FontFace = Font.new("rbxassetid://12187365364")		
 		slidername.Text = Slider.Name
-		slidername.TextColor3 = Color3.fromRGB(115, 115, 115)
+		slidername.TextColor3 = Color3.fromRGB(255, 255, 255)
 		slidername.TextSize = Library.GetScaledTextSize(12)
 		slidername.TextWrapped = true
 		slidername.TextXAlignment = Enum.TextXAlignment.Left
+		slidername.TextYAlignment = Enum.TextYAlignment.Left
 		slidername.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 		slidername.BackgroundTransparency = 1
 		slidername.BorderColor3 = Color3.fromRGB(0, 0, 0)
